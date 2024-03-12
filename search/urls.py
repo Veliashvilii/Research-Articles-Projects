43e511084fadecb3a74247cd6a706dd1381b01e4
Metehan Belli <metehanbelli8@gmail.com>
2024-03-12 13:21:39 +0300

    Improve showResult.html

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index,),  
+  path('', views.index, name='show_main'),  
   path('result/', views.show_result, name='show_result'),
   path('filtered/', views.show_filtered, name='show_filtered'),
   path('detail/', views.detail, name='detail'),
